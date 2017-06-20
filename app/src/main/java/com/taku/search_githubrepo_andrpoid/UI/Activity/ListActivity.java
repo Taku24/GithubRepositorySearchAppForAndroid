0542b56728b35b5d1cd8fd0c8b8b119d4b845696
Taku24 <taku178.24@hotmail.co.jp>
2017-06-20 17:09:12 +0900

    [refactoring] code

--- app/src/main/java/com/taku/search_githubrepo_andrpoid/UI/Activity/ListActivity.java
+++ app/src/main/java/com/taku/search_githubrepo_andrpoid/UI/Activity/ListActivity.java
@@ -4,24 +4,16 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.AdapterView;
 
 import com.taku.search_githubrepo_andrpoid.R;
 import com.taku.search_githubrepo_andrpoid.UI.Adapter.ListAdapter;
 import com.taku.search_githubrepo_andrpoid.ViewModel.Activity.ListViewModel;
 import com.taku.search_githubrepo_andrpoid.databinding.ActivityListBinding;
 
-public class ListActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ListActivity extends AppCompatActivity {
 
     private ActivityListBinding binding;
     private ListViewModel viewModel;
-    private String mQuery = "";
-    private String mSort = "";
-    private String mOrder = "";
-    private boolean isSort = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,64 +27,8 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         binding.recyclerView.setLayoutManager(layoutManager);
         binding.recyclerView.setAdapter(adapter);
-        binding.sortSpinner.setOnItemSelectedListener(this);
-        binding.orderSpinner.setOnItemSelectedListener(this);
-        mOrder = binding.orderSpinner.getSelectedItem().toString();
-        binding.searchView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                mQuery = s.toString();
-                fetchListData();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
         viewModel.onCreate();
 
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long i) {
-        int id = parent.getId();
-        switch (id) {
-            case R.id.sortSpinner:
-                if (binding.sortSpinner.getSelectedItem().toString().equals(getText(R.string.select))) {
-                    mSort = "";
-                    isSort = false;
-                } else {
-                    mSort = binding.sortSpinner.getSelectedItem().toString();
-                    isSort = true;
-                }
-                break;
-
-            case R.id.orderSpinner:
-                mOrder = binding.orderSpinner.getSelectedItem().toString();
-                break;
-
-            default:
-                break;
-        }
-        fetchListData();
-    }
-
-    private void fetchListData() {
-        if (isSort) {
-            viewModel.fetchSortRepoList(mQuery, mSort, mOrder);
-        } else {
-            viewModel.fetchRepoList(mQuery, mOrder);
-        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
--- app/src/main/java/com/taku/search_githubrepo_andrpoid/ViewModel/Activity/ListViewModel.java
+++ app/src/main/java/com/taku/search_githubrepo_andrpoid/ViewModel/Activity/ListViewModel.java
@@ -2,9 +2,17 @@ package com.taku.search_githubrepo_andrpoid.ViewModel.Activity;
 
 import android.content.Context;
 import android.databinding.ObservableArrayList;
+import android.databinding.ObservableField;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Switch;
 
 import com.taku.search_githubrepo_andrpoid.Model.Repo;
 import com.taku.search_githubrepo_andrpoid.Network.RepoAPI;
+import com.taku.search_githubrepo_andrpoid.R;
 import com.taku.search_githubrepo_andrpoid.ViewModel.ViewModel;
 
 import java.util.List;
@@ -16,12 +24,18 @@ import rx.android.schedulers.AndroidSchedulers;
  * Created by TAKU on 2017/06/17.
  */
 
-public class ListViewModel extends ViewModel{
+public class ListViewModel extends ViewModel {
 
     public ObservableArrayList<Repo> mRepoList = new ObservableArrayList<>();
+    private Context mContext;
+    private String mQuery = "";
+    private String mSort = "";
+    private String mOrder = "";
+    private boolean isSort = false;
 
     public ListViewModel(Context context) {
         super(context);
+        mContext = context;
     }
 
     @Override
@@ -29,9 +43,64 @@ public class ListViewModel extends ViewModel{
 
     }
 
-    public void fetchRepoList(String query, String order){
+    public AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long i) {
+            int id = parent.getId();
+            String select = mContext.getResources().getText(R.string.select).toString();
+            switch (id) {
+                case R.id.sortSpinner:
+                    if (!select.equals(parent.getItemAtPosition(position).toString())) {
+                        mSort = parent.getItemAtPosition(position).toString();
+                        isSort = true;
+                    }
+                    break;
+
+                case R.id.orderSpinner:
+                    mOrder = parent.getItemAtPosition(position).toString();
+                    break;
+
+                default:
+                    break;
+            }
+            fetchListData();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+
+    public TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            mQuery = s.toString();
+            fetchListData();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private void fetchListData() {
+        if (isSort) {
+            fetchSortRepoList();
+        } else {
+            fetchRepoList();
+        }
+    }
+
+    private void fetchRepoList() {
         compositeSubscription.add(RepoAPI
-                .fetchRepoList(query, order)
+                .fetchRepoList(mQuery, mOrder)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<List<Repo>>() {
                     @Override
@@ -53,9 +122,9 @@ public class ListViewModel extends ViewModel{
         );
     }
 
-    public void fetchSortRepoList(String query, String sort, String order){
+    private void fetchSortRepoList() {
         compositeSubscription.add(RepoAPI
-                .fetchSortRepoList(query, sort, order)
+                .fetchSortRepoList(mQuery, mSort, mOrder)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<List<Repo>>() {
                     @Override
